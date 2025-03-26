031183d915082898afa0e0d0eb4d8e8794a9776b
Yuxi-Cherish <cherish.tingli@gmail.com>
2025-03-26 17:10:43 +0800

    完成主题色定制化

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import { RouterProvider } from 'react-router-dom'
 import sum from '@/test'
 import router from './router'
 
+// 导入定制主体文件
+import './theme.css'
+
 console.log(sum(1, 5))
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,3 +1,4 @@
+import { Button } from 'antd-mobile'
 import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
@@ -6,6 +7,11 @@ const Layout = () => {
       {/* 二级路由出口 */}
       <Outlet />
       我是layout
+      {/* 测试全局生效样式 */}
+      <Button color="primary">测试全局</Button>
+      <div className="purple">
+        <Button color="primary">测试全局</Button>
+      </div>
     </div>
   )
 }
