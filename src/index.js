f20c61ab9dfb7c420ce2b28904e694779839d5f4
Yuxi-Cherish <cherish.tingli@gmail.com>
2025-03-26 16:50:32 +0800

    整体路由设计配置

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App'
+import { RouterProvider } from 'react-router-dom'
 import sum from '@/test'
+import router from './router'
 
 console.log(sum(1, 5))
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(<RouterProvider router={router} />)
